4eeb081f1b4c1cbb3123af0a766ccad94d910166
고경우 <73161313+kkyoungwoo@users.noreply.github.com>
2026-03-15 18:08:06 +0900

    Add factory registration product and workbench filters

--- content/db-products/products/index.ts
+++ content/db-products/products/index.ts
@@ -4,6 +4,7 @@ import api15134013 from "./api-15134013";
 import api15155139 from "./api-15155139";
 import api15154910 from "./api-15154910";
 import api15136560 from "./api-15136560";
+import api15087611 from "./api-15087611";
 
 export const productCatalog = [
   api15086411,
@@ -12,4 +13,5 @@ export const productCatalog = [
   api15155139,
   api15154910,
   api15136560,
+  api15087611,
 ];
